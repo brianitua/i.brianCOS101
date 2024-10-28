7654e37e7d39e1fa5e5abb6e9c28b3da0ddf86bf
brianitua <brian.itua@pau.edu.ng>
2024-10-28 21:02:46 +0100

    Add files via upload

---
+++ week-3/practice_9/target/debug/deps/practice_9-9008192c2ec7ae2d.d
@@ -0,0 +1,5 @@
+/Users/brian/i.brianCOS101/week-3/practice_9/target/debug/deps/libpractice_9-9008192c2ec7ae2d.rmeta: src/main.rs
+
+/Users/brian/i.brianCOS101/week-3/practice_9/target/debug/deps/practice_9-9008192c2ec7ae2d.d: src/main.rs
+
+src/main.rs:
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+/Users/brian/i.brianCOS101/week-3/practice_9/target/debug/practice_9: /Users/brian/i.brianCOS101/week-3/practice_9/src/main.rs
